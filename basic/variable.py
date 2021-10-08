a2dd6a05fee195c452b846fe864a67ab0b3511f2
Pritom Bhowmik <56447975+pritom02bh@users.noreply.github.com>
2021-10-08 20:54:14 +0600

    v

---
+++ basic/variable.py
@@ -0,0 +1,7 @@
+# Variables: Integer, Float and Boolean
+
+students_count = 1000
+rating = 4.51
+is_graduated = False
+
+print('There are ' + str(students_count) + ' with average rating ' + str(rating))
